7b9300958908816a84ca3ba57b7d8ed31db6050b
JulieAllix <allixjulie@gmail.com>
2020-01-05 18:02:47 +0100

    Created the content of the about page

--- resources/views/about.php
+++ resources/views/about.php
@@ -1,3 +1,22 @@
 <?= view('layout.header'); ?>
 
+<main>
+    <div id="wrapper-about">
+        <section id="picture-JA">
+
+        </section>
+        <section id="presentation-JA">
+            <div id="presentation-text-wrapper">
+                <h1 id="about-me-title">About me</h1>
+                    <article id="about-me-text">
+                        <p>Hello, my name is Julie and I'm a French Web Developper. I love all kind of creative activities: after having experimented drawing, doing Photoshop designs, sewing, I now express my creativity by coding. Feel free to checkout my <a href="https://julieallix.wordpress.com/">Wordpress</a> if you are curious about my Photoshop and sewing creations. I also love dogs and pineapple pizza !</p>
+                        <p>After getting my Master's degree from a Business School, I started my career in 2015 in the Supply Chain as a Distribution Planner for Saint Laurent Paris. Very quickly, I started learning Visual Basics in order to automatize my weekly reports on Excel. That's how I discovered my passion for coding.</p>
+                        <p>I see any coding challenge like a riddle to solve. I'm addicted to the rewarding feeling I get when I solve a bug ! Even pineapple pizza doesn't taste that great.</p>
+                        <p>I started learning on my own on OpenClassrooms.com, and decided in 2019 to pursue my dream of turning this passion into my job. In October 2019, I started a full-time five-months-training with O'clock and moved from Paris to Barcelona.</p>
+                    </article>
+                <div>
+        </section>
+    </div>
+</main>
+
 <?= view('layout.footer'); ?>
