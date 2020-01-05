b3d4e904c1e51990ef3cdef9908eb2263e8ff813
JulieAllix <allixjulie@gmail.com>
2020-01-05 19:00:36 +0100

    Created the structure and content of the training page

--- resources/views/training.php
+++ resources/views/training.php
@@ -1,3 +1,30 @@
 <?= view('layout.header'); ?>
 
+<main>
+<div class="wrapper">
+        <section id="picture-oclock">
+
+        </section>
+        <section id="presentation-oclock">
+            <div class="presentation-text-wrapper">
+                <h1 class="presentation-title-oclock">About my training @ O'clock</h1>
+                    <article class="presentation-text">
+                        <h2 class="presentation-subtitle">A virtual classroom</h2>
+                        <p>Oclock is a French school based on a "teleworking" model : there is a virtual classroom enabling the teachers to teach from home, and the students to learn from home as well. The interface shows the screen & webcam of the teacher, and there is a chat enabling the students to interact with the teacher and the other students. There is no time lost in the public transportations, which means more time to sleep, and more time to study or work on personal projects.</p>
+                        <h2 class="presentation-subtitle">The organization of the class</h2>
+                        <p>Every weekday, there is a theoretical class from 9AM to 3-3.30PM, with 1 hour of lunch break. Then, the students have to work on a "challenge" (practical exercise) by themselves for 2 hours. The challenge is corrected in class the next day.</p>
+                        <p>The training I'm in is based of three months for learning the basics to become a web developer (html, css, php, js), then one month of specialization (Symfony, React, Wordpress). And then one month of full-time team-project (the "Apotheosis").<p>
+                        <p>Each big topic is part of an eight-working-days session.</p>
+                        <h2 class="presentation-subtitle">A training with a lot of practical exercises</h2>
+                        <p>In addition to the two-hours challenges the students get every working-day, in each session, the students always have at least one day of peer-programming, which helps them to learn how to work in teams.<p>
+                        <p>At the end of each session, there is half a day of exercises in order to enable the students to check where they stand in their learning of the developer skills.</p>
+                        <p>And as mentioned above, the training ends with one month of full-time project in teams, in autonomy, to experiment the real work conditions.</p>
+                        <button id="oclock-button"><a href="https://oclock.io/formations/developpeur-web">Learn more about this training (FR)</a></button>
+                        
+                    </article>
+                <div>
+        </section>
+    </div>
+</main>
+
 <?= view('layout.footer'); ?>
