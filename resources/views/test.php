c0eb79aeb734ce691f51ec901911ce301259b90b
JulieAllix <allixjulie@gmail.com>
2020-01-05 15:07:00 +0100

    Created the navbar

--- resources/views/test.php
+++ resources/views/test.php
@@ -27,10 +27,10 @@
          <!-- Ici on ajoute la classe justify-content-md-end pour aligner les éléments de menu sur la droite -->
         <div class="collapse navbar-collapse justify-content-md-end text-center" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="#">Projects <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">About</a>
-            <a class="nav-item nav-link" href="#">My training : O'clock</a>
-            <a class="nav-item nav-link" href="#">Contact</a>
+            <a class="nav-item nav-link active" href="#">Teamback <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Teamfront</a>
+            <a class="nav-item nav-link" href="#">Collaboration</a>
+            <a class="nav-item nav-link" href="#">Ma vie de dev</a>
             <!--<a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>-->
             </div>
         </div>
@@ -42,14 +42,10 @@
     <main>
 
     </main>
+    <footer>
 
+    </footer>
+</body>
+</html>
 
 <!-- < ?= view('layout.footer'); ?> -->
-
-<footer>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    </footer>
-  </body>
-</html>
