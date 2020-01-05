e5f2befafc1851b9347650f4af0d608ad30e499e
JulieAllix <allixjulie@gmail.com>
2020-01-05 15:41:46 +0100

    Basic structure of the homepage done

--- resources/views/home.php
+++ resources/views/home.php
@@ -16,37 +16,51 @@
 </head>
 <body>
     <header>
+        <nav class="navbar navbar-expand-md navbar-light bg-light">
+            <a class="navbar-brand" href="https://github.com/JulieAllix">Julie Allix</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-md-end text-center" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="#">Projects <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="#">About</a>
+                    <a class="nav-item nav-link" href="#">My training : O'clock</a>
+                    <a class="nav-item nav-link" href="#">Contact</a>
+                </div>
+            </div>
+        </nav>
+    </header>
 
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
-        <a class="navbar-brand" href="https://github.com/JulieAllix">Julie Allix</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-         <!-- On ajoute ensuite text-center pour le comportement des éléments s'affichant lors du déploiement du menu burger -->
-         <!-- https://getbootstrap.com/docs/4.3/utilities/text/#text-alignment -->
-         <!-- Ici on ajoute la classe justify-content-md-end pour aligner les éléments de menu sur la droite -->
-        <div class="collapse navbar-collapse justify-content-md-end text-center" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="#">Projects <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">About</a>
-            <a class="nav-item nav-link" href="#">My training : O'clock</a>
-            <a class="nav-item nav-link" href="#">Contact</a>
-            <!--<a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>-->
+    <main>
+        <div class="container-fluid" id="projects-container">
+            <div class="row">
+                <div class="projects col-xl-4 col-lg-12">
+                Study cards
+                </div>
+                <div class="projects col-xl-4 col-lg-12">
+                Break Free : my first game in Js
+                </div>
+                <div class="projects col-xl-4 col-lg-12">
+                This Portfolio
+                </div>
             </div>
         </div>
-    </nav>
-
-
-       
-    </header>
-    <main>
-
     </main>
 
 
 <!-- < ?= view('layout.footer'); ?> -->
 
 <footer>
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="https://github.com/JulieAllix">Github</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="https://linkedin.com/in/julie-allix-391a7431">LinkedIn</a>
+        </li>
+    </ul>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
