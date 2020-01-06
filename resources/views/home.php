4b0c4e392a97e67260b356c3b1a46a32741935a3
JulieAllix <allixjulie@gmail.com>
2020-01-06 18:15:26 +0100

    loaded the pictures

--- resources/views/home.php
+++ resources/views/home.php
@@ -3,10 +3,10 @@
     <main>
         <div class="container-fluid" id="projects-container">
             <div class="row">
-                <div class="projects col-xl-4 col-lg-12">
+                <div class="projects col-xl-4 col-lg-12" id="study-cards">
                 Study cards
                 </div>
-                <div class="projects col-xl-4 col-lg-12">
+                <div class="projects col-xl-4 col-lg-12" id="break-free">
                 Break Free : my first game in Js
                 </div>
                 <div class="projects col-xl-4 col-lg-12">
